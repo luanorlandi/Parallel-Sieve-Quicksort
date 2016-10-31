06468846c8e2e905f9f6c191a732565559603e29
Luan Orlandi <orlandi.luan@gmail.com>
2016-10-31 16:52:37 +0000

    Parallel

--- sieve.c
+++ sieve.c
@@ -2,20 +2,28 @@
 // gcc -g -Wall -fopenmp -o sieve sieve.c -lm
 
 // run with
-// ./sieve 100 0
+// ./sieve 100
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <time.h>
 #include <omp.h>
 
 #define PRIME 0
 #define NONPRIME 1
 
+#define THREAD_COUNT 4
+
 void crossout(int *prime, int size) {
 	int i, j;
 
+	#pragma omp parallel for num_threads(THREAD_COUNT) \
+		private(j)
 	for(i = 2; i < size; i++) {
+
+		/* if is crossout, all his multiple
+			it's already or being crossout */
 		if(prime[i] == PRIME) {
 			for(j = i+i; j < size; j += i) {
 				prime[j] = NONPRIME;
@@ -42,6 +50,7 @@ void showAnswer(int *prime, int size) {
 int main(int argc, char *argv[]) {
 	int *prime;		/* boolean */
 	int size;
+	clock_t t;
 
 	if(argc < 2) {
 		printf("Need argument for range\n");
@@ -55,13 +64,17 @@ int main(int argc, char *argv[]) {
 	/* not prime numbers will be 1 */
 	prime = (int*) calloc(size, sizeof(int));
 
+	t = clock();
+
 	/* put 1 in not primes */
-	//#pragma omp parallel num_threads(4)
 	crossout(prime, size);
 
-	showAnswer(prime, size);
+	t = clock() - t;
 
-	free(prime);
+	showAnswer(prime, size);
+	printf("Crossout done in %.2lfs\n", (double) t / CLOCKS_PER_SEC);
+
+	//free(prime);
 
 	return 0;
 }
