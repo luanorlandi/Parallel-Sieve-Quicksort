8974bc9cc45f99db917f7e829b3f0ebcd1338043
mvjunq <codejunq@gmail.com>
2016-10-31 20:49:08 -0200

    new file:   sieve 	new file:   sieve.7z 	modified:   sieve.c

--- sieve.c
+++ sieve.c
@@ -2,7 +2,19 @@
 // gcc -g -Wall -fopenmp -o sieve sieve.c -lm
 
 // run with
-// ./sieve 100
+// ./sieve n numero_threads
+// onde n eh um intervalo de [0...n]
+
+
+/*
+ * Integrantes:
+ * Marcos Vinicius Barros L. A. J.			8922393
+ * Luan Gustavo Orlandi						8531532
+ * Leonardo Mellin Moreira Ferreira			7982767
+*/
+
+
+
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -13,15 +25,17 @@
 #define PRIME 0
 #define NONPRIME 1
 
-#define THREAD_COUNT 4
+#define THREAD_COUNT 7
 
-void crossout(int *prime, int size) {
-	int i, j;
+/* typedef for  max range of a integer */
+typedef signed long long int slli;
 
-	#pragma omp parallel for num_threads(THREAD_COUNT) \
+void crossout(slli *prime, slli size, int nthreads) {
+
+	slli i, j;
+	#pragma omp parallel for num_threads(nthreads) \
 		private(j)
 	for(i = 2; i < size; i++) {
-
 		/* if is crossout, all his multiple
 			it's already or being crossout */
 		if(prime[i] == PRIME) {
@@ -32,49 +46,54 @@ void crossout(int *prime, int size) {
 	}
 }
 
-void showAnswer(int *prime, int size) {
-	int total = 0, i;
+void showAnswer(slli *prime, slli size) {
 
-	printf("Prime numbers found (2 to %d):\n", size-1);
+	slli total = 0, i;
+
+	printf("Prime numbers found (2 to %lld):\n", size-1);
 
 	for(i = 2; i < size; i++) {
 		if(prime[i] == PRIME) {
-			printf("%d ", i);
+			printf("%lld ", i);
 			total++;
 		}
 	}
 
-	printf("\nTotal: %d\n", total);
+	printf("\nTotal: %lld\n", total);
 }
 
 int main(int argc, char *argv[]) {
-	int *prime;		/* boolean */
-	int size;
+
+	slli *prime;		/* boolean */
+	slli size;
+	int nthreads = 0;
 	clock_t t;
 
-	if(argc < 2) {
-		printf("Need argument for range\n");
+	if(argc < 3) {
+		printf("Need argument for range (r) and numbers of threads(n) ex: ./sieve n r ");
 		exit(-1);
 	}
 
 	/* inclusive range */
-	size = 1 + atoi(argv[1]);
+	size = 1 + atol(argv[1]);
+	nthreads = atoi(argv[2]);
 
 	/* prime numbers will be 0 */
 	/* not prime numbers will be 1 */
-	prime = (int*) calloc(size, sizeof(int));
+	prime = (slli *) calloc(size, sizeof(slli));
 
 	t = clock();
 
 	/* put 1 in not primes */
-	crossout(prime, size);
+	crossout(prime, size, nthreads);
 
 	t = clock() - t;
 
 	showAnswer(prime, size);
+	printf("Numbers of threads used: %d\n", nthreads);
 	printf("Crossout done in %.2lfs\n", (double) t / CLOCKS_PER_SEC);
 
-	//free(prime);
+	free(prime);
 
 	return 0;
 }
