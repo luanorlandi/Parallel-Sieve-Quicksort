5d655235f80ec82ff0ef607979ae706c549d8fcc
lmmf <leonardo.mmferreira@gmail.com>
2016-11-22 10:17:21 -0200

    Add files via upload

--- quicksort.c
+++ quicksort.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <omp.h>
 #include <math.h>
+#include <limits.h>
 
 #include "mpi.h"
 
@@ -11,12 +12,15 @@ int compare(const void * a, const void * b) {
    return(*(int*)a-*(int*)b);
 }
 
-void troca_elementos(int *vetor, int tamanho, int nthreads, int start, int size, int *pivots) {
-	int i, j, k, **envio, rank;
+int *troca_elementos(int *vetor, int tamanho, int nthreads, int start, int size, int *pivots) {
+	int i, j, k, **envio, rank, *recebimento, *valores;
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	recebimento=(int *)malloc(tamanho+1*sizeof(int));
+	valores=(int *)malloc(tamanho+1*sizeof(int));
+	valores[0]=0;
 	envio=(int **)malloc(nthreads*sizeof(int *));
 	for(i=0;i<nthreads;i++) {
-		envio[i]=(int *)malloc(tamanho*sizeof(int));
+		envio[i]=(int *)malloc(tamanho+1*sizeof(int));
 		envio[i][0]=0;
 	}
 	for(i=start;i<start+size;i++) {
@@ -32,15 +36,38 @@ void troca_elementos(int *vetor, int tamanho, int nthreads, int start, int size,
 			envio[nthreads-1][envio[nthreads-1][0]]=vetor[i];
 		}
 	}
-	//codigo para envio e recebimento da matriz envio
+	for(i=0;i<nthreads;i++) {
+		if(i!=rank) {
+			if(MPI_Send(envio[i], tamanho+1, MPI_INT, i, TAG, MPI_COMM_WORLD)!=MPI_SUCCESS) {
+				printf("Houve um erro no envio de uma mensagem.");
+				MPI_Finalize();
+				exit(1);
+			}
+		}
+	}
+	for(i=0;i<nthreads;i++) {
+		if(i!=rank) {
+			MPI_Recv(recebimento, tamanho+1, MPI_INT, MPI_ANY_SOURCE, TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			for(j=0;j<recebimento[0];j++) {
+				valores[valores[0]+1]=recebimento[j];
+				valores[0]++;
+			}
+		}
+	}
+	for(i=1;i<envio[rank][0];i++) {
+		valores[valores[0]+1]=envio[rank][i];
+		valores[0]++;
+	}
 	for(i=0;i<nthreads;i++) {
 		free(envio[i]);
 	}
 	free(envio);
+	free(recebimento);
+	return valores;
 }
 
 void master(int *vetor, int tamanho, int nthreads) {
-	int i, tid, *size, *start, n, t, soma=0, *samples, *pivots, x;
+	int i, tid, *size, *start, n, t, soma=0, *samples, *pivots, x, *valores, aux, *buffer;
 	size=(int *)malloc(nthreads*sizeof(int));
 	start=(int *)malloc(nthreads*sizeof(int));
 	samples=(int *)malloc(nthreads*nthreads*sizeof(int));
@@ -84,10 +111,36 @@ void master(int *vetor, int tamanho, int nthreads) {
 			exit(1);
 		}
 	}
+	valores=troca_elementos(vetor, tamanho, nthreads, start[0], size[0], pivots);
+	#pragma omp parallel private(tid, i, buffer) num_threads(nthreads)
+	{
+		tid=omp_get_thread_num();
+		if(!tid) {
+			aux=valores[0];
+			valores[0]=INT_MAX;
+			qsort(valores, aux+1, sizeof(int), compare);
+			for(i=0;i<size[0];i++) {
+				vetor[i]=valores[i];
+			}
+		}
+		else {
+			buffer=(int *)malloc((tamanho+1)*sizeof(int));
+			MPI_Recv(buffer, tamanho+1, MPI_INT, tid, TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			aux=buffer[0];
+			buffer[0]=INT_MAX;
+			qsort(buffer, aux+1, sizeof(int), compare);
+			for(i=start[tid];i<start[tid]+size[tid];i++) {
+				vetor[start[tid]+i]=buffer[i];
+			}
+			free(buffer);
+		}
+	}
+	free(valores);
 	free(size);
 	free(start);
 	free(samples);
 	free(pivots);
+	free(valores);
 }
 
 void slave(int *vetor, int tamanho, int nthreads) {
@@ -96,8 +149,14 @@ void slave(int *vetor, int tamanho, int nthreads) {
 	MPI_Recv(pivots, nthreads-1, MPI_INT, 0, TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 	MPI_Recv(&start, 1, MPI_INT, 0, TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 	MPI_Recv(&size, 1, MPI_INT, 0, TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-
+	valores=troca_elementos(vetor, tamanho, nthreads, start, size, pivots);
+	if(MPI_Send(valores, tamanho+1, MPI_INT, 0, TAG, MPI_COMM_WORLD)!=MPI_SUCCESS) {
+		printf("Houve um erro no envio de uma mensagem.");
+		MPI_Finalize();
+		exit(1);
+	}
 	free(pivots);
+	free(valores);
 }
 
 int main(int argc, char **argv) {
@@ -113,9 +172,13 @@ int main(int argc, char **argv) {
 	if(!rank) {
 		master(vetor, tamanho, nthreads);
 	}
-	else
+	else {
 		slave(vetor, tamanho, nthreads);
+	}
 	MPI_Finalize();
+	for(i=0;i<tamanho;i++) {
+		printf("%d ", vetor[i]);
+	}
 	free(vetor);
 	return 0;
 }
